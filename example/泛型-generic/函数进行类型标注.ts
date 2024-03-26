12828a98f8a3f1b9bd040b2374090d9acf9d672e
muguilin <muguilin@foxmail.com>
2024-03-26 10:50:30 +0800

    update

--- example/泛型-generic/函数进行类型标注.ts
+++ example/泛型-generic/函数进行类型标注.ts
@@ -2,8 +2,12 @@ function handler(num1: number, num2: number) {
     return num1 + num2;
 }
 
-declare function debounce<A extends any[], R>(fn: (...args: A) => R, duration: number): (...args: A) => R;
+declare function debounce<A extends Array<any> | any[], R>(
+    fn: (...args: A) => R,
+    duration: number
+): (...args: A) => R;
+
 
 const fun = debounce(handler, 1000)
 
-console.log(fun(1, 2));
+console.log(fun, fun(1, 2));
